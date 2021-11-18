4228ae2734f9f0a3b9026981d1526717e75b051a
Paavokal <paavo.kalliala@gmail.com>
2021-11-18 17:39:05 +0200

    part 3 rdy!

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const { response } = require('express')
 const app = express()
 app.use(express.static('build'))
 app.use(express.json())
@@ -11,89 +10,84 @@ app.use(cors())
 
 
 //Morgan
-morgan.token("person", (res) => { 
+morgan.token('person', (res) => {
     return JSON.stringify(res.body)
 })
 app.use(
-    morgan( 
+    morgan(
         ':method :url :status :res[content-length] - :response-time ms :person'
     )
 )
 
 //GET ALL PERSONS
-  app.get('/api/persons', (req, res) => {
-      Person.find({}).then(persons => {
-          res.json(persons)
-      })
-  })
+app.get('/api/persons', (req, res) => {
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
+})
 
-  // INFO PAGE
-  app.get('/info',(req,res) => {
+// INFO PAGE
+app.get('/info',(req,res) => {
     Person.count({},(err,count) => {
         res.send(`Phonebook has info for ${count} people</br></br>${new Date()}`)
-    })   
-  })
+    })
+})
 
 //GET ONE PERSON
-  app.get('/api/persons/:id', (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
         .then(person => {
             if (person) {
                 res.json(person)
-            } 
+            }
             else {
                 res.status(404).end()
-            }      
+            }
         })
         .catch(error => {
-            console.log("WROND ID")
+            console.log('WRONG ID')
             next(error)
         })
-  })
+})
 //DELETE PERSON
-  app.delete('/api/persons/:id', (req, res, next) => {
-      Person.findByIdAndRemove(req.params.id)
-      .then(result => {
-          res.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
 //POST NEW PERSON
-    app.post('/api/persons', (req, res, next) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
-  
     if (body.name === undefined) {
-      return res.status(400).json({ error: 'name missing' })
+        return res.status(400).json({ error: 'name missing' })
     }
-  
     const person = new Person({
         name: body.name,
         number: body.number
     })
-  
     person.save()
-    .then(savedPerson => {
-      res.json(savedPerson)
-    })
-    .catch(error => next(error))
-  })
+        .then(savedPerson => {
+            res.json(savedPerson)
+        })
+        .catch(error => next(error))
+})
 
 //UPDATE PERSON
-  app.put('/api/persons/:id', (req, res, next) => {
+app.put('/api/persons/:id', (req, res, next) => {
     const body = req.body
-  
     const person = {
-      name: body.name,
-      number: body.number
+        name: body.name,
+        number: body.number
     }
-  
     Person.findByIdAndUpdate(req.params.id, person, { new: true })
-      .then(updatedPerson => {
-        res.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
 
 //UNKNOWN ENDPOINT
@@ -111,11 +105,11 @@ console.error(error.message)
     }
     else if (error.name === 'ValidationError') {
         return res.status(400).json({ error: error.message })
-      }
+    }
     else if (error.name === 'MongoServerError') {
         return res.status(400).json({ error: error.message })
-      }
-    
+    }
+
     next(error)
 }
 
